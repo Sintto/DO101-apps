3449677fc00dcb5796fecaf36f26d9acde8a5359
juraj.sintal <>
2022-11-29 08:25:34 +0100

    zovseobecnenie

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://sinto:heslo@localhost:5432/databaza';
+const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://username:password@dbservice:port/dbname';
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
