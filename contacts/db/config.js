ee39367cf768eeca7a3629c2d29a7f7327b1495a
juraj.sintal <>
2022-11-29 08:45:28 +0100

    port

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://sinto:password@contactsdb-service:port/databaza'; //postgresql://username:password@dbservice:port/dbname
+const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://sinto:password@contactsdb-service:5432/databaza'; //postgresql://username:password@dbservice:port/dbname
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
